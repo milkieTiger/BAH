e1e9fc6034efbf50a1d2748ace006292df6d1345
milkieTiger <milkieTiger@users.noreply.github.com>
2026-07-28 16:37:46 +0000

    deploy: 676bd2a0ee554bbd0ca273113445faef2d61896d

---
+++ _next/static/BcUzBFAzKWMkdH19_wcvE/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Factivities\u002F[slug]","\u002Fbooth-listing\u002F[slug]","\u002Fschedule\u002F[slug]","\u002Ftickets\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
